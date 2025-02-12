12a599663c342f78a1e61e93a2a4cbfb5c7c2fab
ArjunS-07 <arjunshivakumar07@gemail.com>
2025-02-12 09:52:04 +0530

    level2_design

---
+++ day2 react/.--/src/App.jsx
@@ -0,0 +1,15 @@
+import CarCreate from "./carparking/CarCreate";
+import CarsList from "./carparking/CarsList";
+import CarView from "./carparking/CarView";
+
+export default function App(){
+
+  return(
+    <>
+    <CarsList/>
+    <CarView/>
+    <CarCreate/>
+    </>
+  )
+}
+
